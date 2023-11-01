931c7c73cb2902b805462cf5cef42d03c58e5f37
Luiz Henrique <oni.master.232084@outlook.com>
2023-11-01 15:41:48 -0300

    Adição de valor de aposta

--- main.py
+++ main.py
@@ -1,33 +1,37 @@
 import random
 
-carteira = 100
+carteira = float(input("Quanto você possui na sua carteira ?: "))
+aposta = float(input("Quanto você deseja apostar ?: "))
 
 print("### Máquina caça-níqueis ###\n")
 print(f"Você possui R${carteira}\n")
 
-itens = ('Sino', 'Cereja', 'Sete', 'Bar')
+if carteira < aposta:
+    print("Você não tem dinheiro para apostar")
+else:
+    itens = ('Sino', 'Cereja', 'Sete', 'Bar')
 
-x = random.sample(itens, 3)
-j = itens[2], itens[2], itens[2]
-t = itens[0], itens[0], itens[0]
-d = itens[3], itens[3], itens[3]
+    x = random.sample(itens, 3)
+    j = itens[2], itens[2], itens[2]
+    t = itens[0], itens[0], itens[0]
+    d = itens[3], itens[3], itens[3]
 
-possibilidades = (x, j, t, d)
-probabelilidades = (10, 0.5, 0.8, 1)
+    possibilidades = (x, j, t, d)
+    probabelilidades = (10, 0.5, 0.8, 1)
 
-resultado = random.choices(possibilidades, probabelilidades)[0]
+    resultado = random.choices(possibilidades, probabelilidades)[0]
 
-print(resultado)
+    print(resultado)
 
-if resultado == x:
-    print("Tente de novo!")
-    print(carteira - 50)
-if resultado == j:
-    print("JACKPOT!!!")
-    print(carteira * 10000)
-if resultado == t:
-    print("Você ganhou 3X")
-    print(carteira * 3)
-if resultado == d:
-    print("Você ganhou 2X")
-    print(carteira * 2)
+    if resultado == x:
+        print("Tente de novo!")
+        print(f"R${carteira - aposta}")
+    if resultado == j:
+        print("JACKPOT!!!")
+        print(f"R${carteira + aposta * 10000}")
+    if resultado == t:
+        print("Você ganhou 3X")
+        print(f"R${carteira + aposta * 3}")
+    if resultado == d:
+        print("Você ganhou 2X")
+        print(f"R${carteira + aposta * 2}")
